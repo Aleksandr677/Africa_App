3800c787091dd250d25aa5644a70254de81ef0a3
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-10-27 20:09:14 +0300

    feat: create CoverImageView, parse local JSON file, create an extention to Bundle

---
+++ Africa_App/Africa_App/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa_App
+//
+//  Created by Христиченко Александр on 2022-10-27.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
