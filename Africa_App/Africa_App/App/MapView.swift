31f01b30453d041bdef6b6059b2043859ab9046b
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-10-29 15:19:03 +0300

    feat: add MapAnnotationView, modify MapView

--- Africa_App/Africa_App/App/MapView.swift
+++ Africa_App/Africa_App/App/MapView.swift
@@ -25,20 +25,75 @@ struct MapView: View {
         //Map(coordinateRegion: $region)
         
         //MARK: - No2 ADVANCED MAP
-        Map(coordinateRegion: $region, annotationItems: locations) { item in
-            //(A) PIN: OLD STYLE (always static)
-            //MapPin(coordinate: item.location, tint: .accentColor)
-            
-            //(B) MARKER: NEW STYLE (always static)
-            //MapMarker(coordinate: item.location, tint: .accentColor)
-            
-            //(C) CUSTOM BASIC ANNOTATION (it could be interactive)
-            MapAnnotation(coordinate: item.location) {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
+        if #available(iOS 15.0, *) {
+            Map(coordinateRegion: $region, annotationItems: locations) { item in
+                //(A) PIN: OLD STYLE (always static)
+                //MapPin(coordinate: item.location, tint: .accentColor)
+                
+                //(B) MARKER: NEW STYLE (always static)
+                //MapMarker(coordinate: item.location, tint: .accentColor)
+                
+                //(C) CUSTOM BASIC ANNOTATION (it could be interactive)
+                //            MapAnnotation(coordinate: item.location) {
+                //                Image("logo")
+                //                    .resizable()
+                //                    .scaledToFit()
+                //                    .frame(width: 32, height: 32, alignment: .center)
+                //            }
+                
+                // (D) CUSTOM ADVANCED ANNOTATION (it could be interactive)
+                MapAnnotation(coordinate: item.location) {
+                    MapAnnotationView(location: item)
+                }
+            } //MAP
+            .overlay(alignment: .top) {
+                HStack(alignment: .center, spacing: 12) {
+                    Image("compass")
+                        .resizable()
+                        .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                    
+                    VStack(alignment: .leading, spacing: 3) {
+                        HStack {
+                            Text("Latitude:")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .foregroundColor(.accentColor)
+                            
+                            Spacer()
+                            
+                            Text("\(region.center.latitude)")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Longitude:")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .foregroundColor(.accentColor)
+                            
+                            Spacer()
+                            
+                            Text("\(region.center.longitude)")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                        }
+                    }
+                } //HStack
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
             }
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
